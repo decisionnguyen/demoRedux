ef76722f04ba6ac76f397746826db5bbbf1d75cf
decisionnguyen <decisionnguyen@gmail.com>
2022-03-23 14:48:12 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,24 @@
+import 'react-redux';
+import {StateType} from 'typesafe-actions';
+
 export interface RawTodo {
     id: string,
     name: string,
     value: string
+}
+
+
+
+export type RootState = StateType<typeof import('./rootReducer').default>;
+
+declare module 'react-redux' {
+  export type EqualityFnType<TSelected> = (
+    left: TSelected,
+    right: TSelected,
+  ) => boolean;
+
+  export function useSelector<TSelected>(
+    selector: (state: RootState) => TSelected,
+    equalityFn?: EqualityFnType<TSelected>,
+  ): TSelected;
 }
